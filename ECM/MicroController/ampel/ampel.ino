4c820e87ca64ba74b7591240a993abc72199fc0e
Maximilian Philipp <philipp@student.tugraz.at>
2022-06-15 14:04:40 +0200

    removed magic number

--- ECM/MicroController/ampel/ampel.ino
+++ ECM/MicroController/ampel/ampel.ino
@@ -5,6 +5,7 @@
 #define A1_Y 12
 #define A1_G 11
 #define LIGHT_SENSOR 19
+#define NUM_WARN_BLINKS 4 
 
 char pins[5] = {A2_G, A2_R, A1_R, A1_Y, A1_G};
 const byte interruptPin = 2;
@@ -88,7 +89,7 @@ void loop() {
 
 void schaltSequenz_() {
   // Green Blinking phase
-  for (size_t i = 0; i < 4; i++) {
+  for (size_t i = 0; i < NUM_WARN_BLINKS; i++) {
     digitalWrite(A1_G, LOW);
     delay(1000);
     digitalWrite(A1_G, HIGH);
@@ -105,7 +106,7 @@ void schaltSequenz_() {
   digitalWrite(A2_G, HIGH);
   delay(5000);
   // Green Blinking phase for walkers
-  for (size_t i = 0; i < 4; i++) {
+  for (size_t i = 0; i < NUM_WARN_BLINKS; i++) {
     digitalWrite(A2_G, LOW);
     delay(1000);
     digitalWrite(A2_G, HIGH);
